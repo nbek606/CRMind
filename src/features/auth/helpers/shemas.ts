7a741413ab3e26e40ddd45ff306f26330a94175f
unknown <@kubatov>
2025-03-09 22:52:53 +0600

    fix-registr-auth

--- src/features/auth/helpers/shemas.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-
-export const authFormSchema = z.object({
-    username: z.string().min(1, "Поле не должно быть пустым"),
-    password: z.string().min(1, "Поле не должно быть пустым"),
-});
-
-export type TAuthFormSchema = typeof authFormSchema
-export type TAuthFormData = z.infer<TAuthFormSchema>
